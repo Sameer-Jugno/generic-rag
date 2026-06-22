f47e92336e15bc8cba3539e56128c3eeee9d89d1
sameerjugno <sameerjugno3@gmail.com>
2026-06-22 11:20:04 +0500

    feat: implement batched pointstruct and embedding data vector generation

--- backend/services/vector_db.py
+++ backend/services/vector_db.py
@@ -1,5 +1,8 @@
 import fitz 
+import uuid
+from qdrant_client.models import PointStruct 
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
+from sentence_transformers import SentenceTransformer
 
 def extract_text_from_pdf(file_bytes: bytes) -> list:
     """Uses PyMuPDF (fitz) to read raw byte streams in-memory and extract text."""
@@ -28,7 +31,33 @@ def chunk_text(chunks : list, chunk_size: int = 1000, overlap: int = 200) -> lis
 
 def generate_embeddings(chunks: list) -> tuple:
     """Uses SentenceTransformers to turn text strings into 1024-dimension float vectors."""
-    pass
+    embedder = SentenceTransformer(model="BAAI/bge-m3")
+    batch = 32 
+    batches = []
+    for i in range(0,len(chunks), batch) :   
+        batched_chunks = chunks[i:i+batch] 
+        batched_texts = [chunk.page_content for chunk in batched_chunks] 
+
+        vectors = embedder.encode(batched_texts).tolist()  
+
+        for chunk, vector in zip(batched_chunks, vectors) : 
+            id = str(uuid.uuid4())
+            payload = {
+                "page_content" : chunk.page_content,
+                "metadata" : chunk.metadata
+            }
+
+            point = PointStruct(
+                id=id, 
+                vector=vector,
+                payload=payload
+            )
+            batches.append(point)
+    
+    return batches 
+    # text = embedder.encode_document(texts, precision="float32", show_progress_bar=True).tolist()
+
+        
 
 def initialize_qdrant_collection(collection_name: str):
     """Connects via API to provision a brand-new private collection table on Qdrant Cloud."""
